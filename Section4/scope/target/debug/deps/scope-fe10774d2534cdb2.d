db8ee345660d45facf06453b2bf082785992adcf
jiruneko <oohikaru310@gmail.com>
2025-12-03 09:37:38 +0900

    second commit

---
+++ Section4/scope/target/debug/deps/scope-fe10774d2534cdb2.d
@@ -0,0 +1,5 @@
+/Users/nagatsukakenji/Rust_tutorial/Section4/scope/target/debug/deps/scope-fe10774d2534cdb2.d: src/main.rs
+
+/Users/nagatsukakenji/Rust_tutorial/Section4/scope/target/debug/deps/scope-fe10774d2534cdb2: src/main.rs
+
+src/main.rs:
---
+++ Section4/scope/target/debug/scope.d
@@ -0,0 +1,1 @@
+/Users/nagatsukakenji/Rust_tutorial/Section4/scope/target/debug/scope: /Users/nagatsukakenji/Rust_tutorial/Section4/scope/src/main.rs

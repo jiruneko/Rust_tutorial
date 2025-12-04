fa4e59b504d721c8c1c5ba3e3f579df0865c5efa
jiruneko <oohikaru310@gmail.com>
2025-12-04 11:52:19 +0900

    third commit

---
+++ Section3/function/target/debug/deps/function-740896896eedc54e.d
@@ -0,0 +1,5 @@
+/Users/nagatsukakenji/Rust_tutorial/Section3/function/target/debug/deps/function-740896896eedc54e.d: src/main.rs
+
+/Users/nagatsukakenji/Rust_tutorial/Section3/function/target/debug/deps/function-740896896eedc54e: src/main.rs
+
+src/main.rs:

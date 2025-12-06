d950787d2630ee4f4ba8544006405b1be1c539a2
jiruneko <oohikaru310@gmail.com>
2025-12-07 05:20:20 +0900

    Add new feature

---
+++ Section5/structure/target/debug/deps/structure-56472efb3bb40ea8.d
@@ -0,0 +1,5 @@
+/Users/nagatsukakenji/Rust_tutorial/Section5/structure/target/debug/deps/structure-56472efb3bb40ea8.d: src/main.rs
+
+/Users/nagatsukakenji/Rust_tutorial/Section5/structure/target/debug/deps/structure-56472efb3bb40ea8: src/main.rs
+
+src/main.rs:
---
+++ Section5/structure/target/debug/structure.d
@@ -0,0 +1,1 @@
+/Users/nagatsukakenji/Rust_tutorial/Section5/structure/target/debug/structure: /Users/nagatsukakenji/Rust_tutorial/Section5/structure/src/main.rs

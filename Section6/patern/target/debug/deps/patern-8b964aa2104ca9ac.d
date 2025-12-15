a0dd7ab69ee35d1ce9074336e5eb51083cf5de07
jiruneko <oohikaru310@gmail.com>
2025-12-16 08:51:32 +0900

    Add new feature

---
+++ Section6/patern/target/debug/deps/patern-8b964aa2104ca9ac.d
@@ -0,0 +1,5 @@
+/Users/nagatsukakenji/Rust_tutorial/Section6/patern/target/debug/deps/patern-8b964aa2104ca9ac.d: src/main.rs
+
+/Users/nagatsukakenji/Rust_tutorial/Section6/patern/target/debug/deps/patern-8b964aa2104ca9ac: src/main.rs
+
+src/main.rs:
